3492fad39994cce9b9c06e782d5107f6ec9d9159
IMOitself <russjonn@gmail.com>
2025-06-05 03:05:49 +0800

    refactor: separate UI from MainActivity.kt

--- app/src/main/java/imo/puppygit/MainActivity.kt
+++ app/src/main/java/imo/puppygit/MainActivity.kt
@@ -4,51 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.AccountTree
-import androidx.compose.material.icons.filled.Android
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Code
-import androidx.compose.material.icons.filled.Download
-import androidx.compose.material.icons.filled.Folder
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreHoriz
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -56,16 +15,14 @@ import java.io.InputStream
 
 
 class MainActivity : ComponentActivity() {
-    private val buttonBorderColor = Color(0xFF9198A1)
-    private val buttonBorderRadius = 4.dp
-    private val darkColorPalette = darkColorScheme(
-        primary = Color(0xFF010409),
-        primaryContainer = Color(0xFF0D1117),
-        secondaryContainer = Color(0xFF999999),
-        tertiary = Color(0xFF29903B)
-    )
-    private var showBottomBar = true
-    private var appNameRetrieve: String? = null
+    companion object {
+        var showBottomBar = true
+        var appNameRetrieve: String? = null
+        fun goToMainActivity(context: Context?){
+            val intent = Intent(context , com.catpuppyapp.puppygit.play.pro.MainActivity::class.java)
+            context?.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,19 +31,15 @@ class MainActivity : ComponentActivity() {
         if (apkFileUri != null) appNameRetrieve = getAppNameFromApkUri(this, apkFileUri)
         if (apkFileUri == null) showBottomBar = false
 
-        // TODO: separate UI to code
         setContent {
-            MaterialTheme(
-                colorScheme = darkColorPalette
-            ) {
-                MainScreen()
-            }
+            MainScreen()
         }
     }
 
-    private fun goToMainActivity(){
-        val intent = Intent(this, com.catpuppyapp.puppygit.play.pro.MainActivity::class.java)
-        startActivity(intent)
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        MainScreen()
     }
 
     fun getAppNameFromApkUri(context: Context, apkUri: Uri?): String? {
@@ -103,33 +56,33 @@ class MainActivity : ComponentActivity() {
                 apkPath = apkUri.path
                 if (apkPath == null) return null
             } else
-            if ("content" == scheme) {
-                var inputStream: InputStream? = null
-                try {
-                    inputStream = context.contentResolver.openInputStream(apkUri)
-                    if (inputStream == null) return null
+                if ("content" == scheme) {
+                    var inputStream: InputStream? = null
+                    try {
+                        inputStream = context.contentResolver.openInputStream(apkUri)
+                        if (inputStream == null) return null
 
-                    tempFile = File.createTempFile("apk_info_", ".apk", context.cacheDir)
-                    FileOutputStream(tempFile).use { outputStream ->
-                        val buffer = ByteArray(4096)
-                        var bytesRead: Int
-                        while ((inputStream.read(buffer).also { bytesRead = it }) != -1) {
-                            outputStream.write(buffer, 0, bytesRead)
+                        tempFile = File.createTempFile("apk_info_", ".apk", context.cacheDir)
+                        FileOutputStream(tempFile).use { outputStream ->
+                            val buffer = ByteArray(4096)
+                            var bytesRead: Int
+                            while ((inputStream.read(buffer).also { bytesRead = it }) != -1) {
+                                outputStream.write(buffer, 0, bytesRead)
+                            }
                         }
-                    }
-                    apkPath = tempFile.absolutePath
-                    isTempFileUsed = true
-                } finally {
-                    if (inputStream != null) {
-                        try {
-                            inputStream.close()
-                        } catch (ignored: IOException) {
-                            // Intentionally ignored as per requirements
+                        apkPath = tempFile.absolutePath
+                        isTempFileUsed = true
+                    } finally {
+                        if (inputStream != null) {
+                            try {
+                                inputStream.close()
+                            } catch (ignored: IOException) {
+                                // Intentionally ignored as per requirements
+                            }
                         }
                     }
                 }
-            }
-            else return null
+                else return null
 
             if (apkPath == null) return null
 
@@ -151,217 +104,4 @@ class MainActivity : ComponentActivity() {
         }
         return null
     }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        MaterialTheme(
-            colorScheme = darkColorPalette
-        ) {
-            MainScreen()
-        }
-    }
-
-    @Composable
-    fun MainScreen() {
-        Scaffold(
-            topBar = { TopBar() },
-            bottomBar = { if (showBottomBar) BottomBar() }
-        )
-        { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .background(color = MaterialTheme.colorScheme.primaryContainer)
-                    .padding(paddingValues)
-                    .fillMaxSize()
-                    .fillMaxHeight(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    BranchSection()
-                    Spacer(Modifier.weight(1f))
-                    ActionSection()
-                }
-                Spacer(modifier = Modifier.height(64.dp))
-                Text(
-                    text = "Show Changed files here\n\n¯\\_(ツ)_/¯",
-                    style = MaterialTheme.typography.bodyMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-    }
-
-    @OptIn(ExperimentalLayoutApi::class)
-    @Composable
-    fun TopBar() {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colorScheme.primary)
-                .padding(8.dp)
-                .padding(top = 8.dp, bottom = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // menu_icon
-            IconButton(onClick = { goToMainActivity() }, modifier = Modifier
-                .defaultButtonBg()
-                .size(32.dp)) {
-                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.Folder, contentDescription = "App Icon", modifier = Modifier.size(32.dp))
-            Spacer(modifier = Modifier.width(8.dp))
-
-            FlowRow(modifier = Modifier.weight(1.0f)) {
-                Text(
-                    text = "ParentFolderPath / ",
-                    style = MaterialTheme.typography.bodySmall,
-                    maxLines = 1
-                )
-                Text(
-                    text = "RepositoryLabel",
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.bodySmall,
-                    maxLines = 1
-                )
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-
-            // search_icon
-            IconButton(onClick = { /* TODO */ }, modifier = Modifier
-                .defaultButtonBg()
-                .size(32.dp)) {
-                Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-
-            // settings_icon
-            IconButton(onClick = { /* TODO */ }, modifier = Modifier
-                .defaultButtonBg()
-                .size(32.dp)) {
-                Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-
-            // profile_icon
-            IconButton(onClick = { /* TODO */ }, modifier = Modifier.size(32.dp)) {
-                Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "Profile")
-            }
-        }
-    }
-
-    @OptIn(ExperimentalLayoutApi::class)
-    @Composable
-    fun BottomBar() {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colorScheme.primary)
-                .padding(8.dp)
-                .padding(top = 8.dp, bottom = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.Android, contentDescription = "Apk", modifier = Modifier.size(32.dp))
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Text(
-                text = "$appNameRetrieve",
-                style = MaterialTheme.typography.bodySmall,
-                maxLines = 1,
-                modifier = Modifier.weight(1.0f)
-            )
-            Row(
-                modifier = Modifier
-                    .height(32.dp)
-                    .highlightButtonBg(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Spacer(Modifier.width(8.dp))
-                Icon(
-                    imageVector = Icons.Filled.Download,
-                    contentDescription = "Install",
-                    modifier = Modifier.size(16.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "Continue Install Apk", style = MaterialTheme.typography.bodySmall)
-                Spacer(modifier = Modifier.width(8.dp))
-            }
-            }
-    }
-
-    @Composable
-    fun BranchSection() {
-        Row(
-            modifier = Modifier
-                .height(32.dp)
-                .defaultButtonBg(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.AccountTree, contentDescription = "Branch", modifier = Modifier.size(16.dp))
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "master", style = MaterialTheme.typography.bodySmall)
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "Dropdown", modifier = Modifier.size(16.dp))
-
-            Spacer(Modifier.width(8.dp))
-        }
-    }
-
-    @Composable
-    fun ActionSection() {
-        Row(
-            modifier = Modifier
-                .height(32.dp)
-                .highlightButtonBg(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.Code, contentDescription = "Command", modifier = Modifier.size(16.dp))
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "Actions", style = MaterialTheme.typography.bodySmall)
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "Dropdown", modifier = Modifier.size(16.dp))
-            Spacer(Modifier.width(8.dp))
-        }
-        Spacer(Modifier.width(8.dp))
-        IconButton(onClick = { /* TODO */ }, modifier = Modifier
-            .defaultButtonBg()
-            .size(32.dp)) {
-            Icon(imageVector = Icons.Filled.MoreHoriz, contentDescription = "More Actions", modifier = Modifier.fillMaxSize())
-        }
-    }
-
-    @Composable
-    private fun Modifier.highlightButtonBg(): Modifier = this.then(
-        Modifier
-            .background(
-                color = MaterialTheme.colorScheme.tertiary,
-                shape = RoundedCornerShape(buttonBorderRadius)
-            )
-            .border(
-                border = BorderStroke(0.dp, Color(0x00000000)),
-                shape = RoundedCornerShape(buttonBorderRadius)
-            )
-    )
-    private fun Modifier.defaultButtonBg(): Modifier = this.then(
-        Modifier
-            .background(
-                color = Color(0x00000000),
-                shape = RoundedCornerShape(buttonBorderRadius)
-            )
-            .border(
-                border = BorderStroke(0.25.dp, buttonBorderColor),
-                shape = RoundedCornerShape(buttonBorderRadius)
-            )
-    )
 }
---
+++ app/src/main/java/imo/puppygit/MainScreen.kt
@@ -0,0 +1,262 @@
+package imo.puppygit
+
+import android.content.Context
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AccountTree
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainScreen() {
+    val context: Context? = LocalContext.current
+    MaterialTheme(
+        colorScheme = darkColorPalette
+    ) {
+        Scaffold(
+            topBar = { TopBar(context) },
+            bottomBar = { if (MainActivity.showBottomBar) BottomBar(context) }
+        )
+        { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.primaryContainer)
+                    .padding(paddingValues)
+                    .fillMaxSize()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    BranchSection(context)
+                    Spacer(Modifier.weight(1f))
+                    ActionSection(context)
+                }
+                Spacer(modifier = Modifier.height(64.dp))
+                Text(
+                    text = "Show Changed files here\n\n¯\\_(ツ)_/¯",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun TopBar(context: Context?) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.primary)
+            .padding(8.dp)
+            .padding(top = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // menu_icon
+        IconButton(onClick = { MainActivity.goToMainActivity(context) }, modifier = Modifier
+            .defaultButtonBg()
+            .size(32.dp)) {
+            Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.Folder, contentDescription = "App Icon", modifier = Modifier.size(32.dp))
+        Spacer(modifier = Modifier.width(8.dp))
+
+        FlowRow(modifier = Modifier.weight(1.0f)) {
+            Text(
+                text = "ParentFolderPath / ",
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1
+            )
+            Text(
+                text = "RepositoryLabel",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // search_icon
+        IconButton(onClick = { /* TODO */ }, modifier = Modifier
+            .defaultButtonBg()
+            .size(32.dp)) {
+            Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // settings_icon
+        IconButton(onClick = { /* TODO */ }, modifier = Modifier
+            .defaultButtonBg()
+            .size(32.dp)) {
+            Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // profile_icon
+        IconButton(onClick = { /* TODO */ }, modifier = Modifier.size(32.dp)) {
+            Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "Profile")
+        }
+    }
+}
+
+@Composable
+fun BottomBar(context: Context?) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.primary)
+            .padding(8.dp)
+            .padding(top = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.Android, contentDescription = "Apk", modifier = Modifier.size(32.dp))
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = "$MainActivity.appNameRetrieve",
+            style = MaterialTheme.typography.bodySmall,
+            maxLines = 1,
+            modifier = Modifier.weight(1.0f)
+        )
+        Row(
+            modifier = Modifier
+                .height(32.dp)
+                .highlightButtonBg(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Spacer(Modifier.width(8.dp))
+            Icon(
+                imageVector = Icons.Filled.Download,
+                contentDescription = "Install",
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Continue Install Apk", style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    }
+}
+
+@Composable
+fun BranchSection(context: Context?) {
+    Row(
+        modifier = Modifier
+            .height(32.dp)
+            .defaultButtonBg(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.AccountTree, contentDescription = "Branch", modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "master", style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "Dropdown", modifier = Modifier.size(16.dp))
+
+        Spacer(Modifier.width(8.dp))
+    }
+}
+
+@Composable
+fun ActionSection(context: Context?) {
+    Row(
+        modifier = Modifier
+            .height(32.dp)
+            .highlightButtonBg(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.Code, contentDescription = "Command", modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "Actions", style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "Dropdown", modifier = Modifier.size(16.dp))
+        Spacer(Modifier.width(8.dp))
+    }
+    Spacer(Modifier.width(8.dp))
+    IconButton(onClick = { /* TODO */ }, modifier = Modifier
+        .defaultButtonBg()
+        .size(32.dp)) {
+        Icon(imageVector = Icons.Filled.MoreHoriz, contentDescription = "More Actions", modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+private fun Modifier.highlightButtonBg(): Modifier = this.then(
+    Modifier
+        .background(
+            color = MaterialTheme.colorScheme.tertiary,
+            shape = RoundedCornerShape(buttonBorderRadius)
+        )
+        .border(
+            border = BorderStroke(0.dp, Color(0x00000000)),
+            shape = RoundedCornerShape(buttonBorderRadius)
+        )
+)
+private fun Modifier.defaultButtonBg(): Modifier = this.then(
+    Modifier
+        .background(
+            color = Color(0x00000000),
+            shape = RoundedCornerShape(buttonBorderRadius)
+        )
+        .border(
+            border = BorderStroke(0.25.dp, buttonBorderColor),
+            shape = RoundedCornerShape(buttonBorderRadius)
+        )
+)
+
+private val buttonBorderColor = Color(0xFF9198A1)
+private val buttonBorderRadius = 4.dp
+private val darkColorPalette = darkColorScheme(
+    primary = Color(0xFF010409),
+    primaryContainer = Color(0xFF0D1117),
+    secondaryContainer = Color(0xFF999999),
+    tertiary = Color(0xFF29903B)
+)
